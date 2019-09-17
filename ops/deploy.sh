0e4526ce23fa2fef008278d39bc9ccbf1a0c2390
Issei Naruta <naruta@cookpad.com>
2019-09-17 19:30:34 +0900

    Update

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -4,10 +4,10 @@ TARGET_HOST=10.0.0.212
 
 ssh -T isu-app-01 << EOSSH
 set -x
-cd /home/isucon/src/isucon9-qualify
+cd /home/isucon/git
 git pull origin master
-rsync -avz /home/isucon/src/isucon9-qualify/webapp/ruby/ /home/isucon/isucari/webapp/ruby/
+sudo cp /home/isucon/git/nginx.conf /etc/nginx/nginx.conf
 sudo systemctl restart isucari.ruby.service
-sudo systemctl restart nginx.service
-sudo systemctl status nginx.service isucari.ruby.service
+sudo nginx -t
+sudo nginx -s reload
 EOSSH
