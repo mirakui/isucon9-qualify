7cd396fbca0ef4951f8656ee80fce9702b29f4d3
Issei Naruta <naruta@cookpad.com>
2019-09-17 19:34:30 +0900

    Update

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -6,6 +6,8 @@ ssh -T isu-app-01 << EOSSH
 set -x
 cd /home/isucon/git
 git pull origin master
+cd /home/isucon/git/webapp/ruby
+bundle
 sudo cp /home/isucon/git/nginx.conf /etc/nginx/nginx.conf
 sudo systemctl restart isucari.ruby.service
 sudo nginx -t
