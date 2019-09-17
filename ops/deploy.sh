8bbc98325d1968f93c9b88a5b98ea5151fdc92fb
Issei Naruta <naruta@cookpad.com>
2019-09-17 20:30:52 +0900

    truncate logs

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -9,6 +9,14 @@ git pull origin master
 cd /home/isucon/git/webapp/ruby
 bundle
 sudo cp /home/isucon/git/nginx.conf /etc/nginx/nginx.conf
+
+sudo bash -c '
+echo > /var/log/nginx/access.log;
+echo > /var/log/nginx/error.log;
+echo > /tmp/isu-rack.log;
+echo > /tmp/isu-rack.systemd.log;
+'
+
 sudo systemctl restart isucari.ruby.service
 sudo nginx -t
 sudo nginx -s reload
