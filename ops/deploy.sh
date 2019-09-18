8649f6a55ccf4ddbdf16c0c6d7ecea428f49a6c7
Issei Naruta <naruta@cookpad.com>
2019-09-18 13:35:56 +0900

    fix

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -7,15 +7,15 @@ set -x
 cd /home/isucon/git
 git pull origin master
 cd /home/isucon/git/webapp/ruby
-bundle
+/home/isucon/ruby/bin/bundle install --path=.bundle
 sudo cp /home/isucon/git/nginx.conf /etc/nginx/nginx.conf
 
 sudo bash -c '
 echo > /var/log/nginx/access.log;
 echo > /var/log/nginx/error.log;
+'
 echo > /tmp/isu-rack.log;
 echo > /tmp/isu-rack.systemd.log;
-'
 
 sudo systemctl restart isucari.ruby.service
 sudo nginx -t
